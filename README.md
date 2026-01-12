506d3c88cebad4487c5ffd1939a272eb13e34500
Gamesco-1 <lucas.rumann@gmx.de>
2026-01-12 02:28:52 +0100

    Upload current state of movement Demo for Blog

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+## Hinweise zum aktuellen Stand und starten der Demo
 
+Da ich aktuell noch in der Entwicklung der Demo bin habe ich sie noch nicht in eine .jar für Minecraft kompiliert, und muss aus der IDE herraus gestartet werden.
+
+Anleitung unter InteliJ:
+
+Es wird InteliJ und eine Java JDK mit mindestens Java21 benötigt.
+Nach Öffnen des Projekts in der IDE muss das Gradle Projekt gebaut werden, das kann mehrere Minuten dauern. 
+Wenn das abgeschlossen ist kann man auf der rechten Seite im Gradle Tab unter MMMIGr12 -> Tasks -> forgegradle runs auf "runClient" gedoppelklickt werden. 
+Dannach sollte sich eine Minecraft Version 21.1 Instanz öffnen die die Mod installiert hat.
+
+Wenn man eine Singleplayer Welt gestartet hat, einfach aus dem Creative Menü *ein* Item in die Hotbar legen und dieses in die Hand nehmen um nach vorne zu laufen, oder ducken um nach oben zu schauen.
---
+++ src/main/java/net/Gamesco/MovementDemo/MovementDemo.java
@@ -0,0 +1,91 @@
+//Main Class for programming the demo mod for our MMMI project
+
+package net.Gamesco.MovementDemo;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MovementDemo.MOD_ID)
+public class MovementDemo {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "movementdemo";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public MovementDemo(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+
+    @SubscribeEvent
+    public void playerTick(TickEvent.PlayerTickEvent event)
+    {
+        //Sets first Item in Inventory(after position) as Item that triggers the automatic walking(Still trying to understand how to specify Items....)
+        Item test = event.player.getInventory().items.getFirst().getItem();
+
+        //Player only walks when previously set Item is in Hand and the Player is on the ground(or else we reeeeally accelerate)
+        if((event.player.isHolding(test))&&(event.player.onGround())){
+
+            //Make the player walk forward in standard walking speed.
+            event.player.moveRelative(0.1f,new Vec3(0,0,0.5f));
+        }
+
+        //Player only looks up when crouching
+        if (event.player.isCrouching()){
+            //Make the Player look up, although still quite "stuttery"
+            event.player.setXRot(event.player.getXRot()-0.2f);
+        }
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // EventBusSubscriber is used to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
